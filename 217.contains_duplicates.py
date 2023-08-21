d3116a56c4a378fa590ee51749b4042e83a09dcc
Nagababu Katakam <naga@Nagababus-MacBook-Air.local>
2023-08-21 13:20:36 -0400

    217 updated the input parameters

--- 217.contains_duplicates.py
+++ 217.contains_duplicates.py
@@ -3,12 +3,17 @@
 # if duplicates, yes
 # if not , false
 
-class solution:
-    def contains_duplicates():
-        unique_nums = set()
-
-        for num in nums:
-            if num in unique_nums:
-                return ("true")
-            unique_nums.add(num)
-        return("false")
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # visted set
+        visited = set()
+        # go through the nums, if not visited, add
+        # if already visited return true
+        for n in nums:
+            if n in visited:
+                return True
+            visited.add(n)
+        return False
+    
+# IO parameters to the function
+        
